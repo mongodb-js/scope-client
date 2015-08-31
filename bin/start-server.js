5b10feeb9c6d9f71f965e2b9d117e778f44bbcce
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-30 21:55:27 -0400

    start/stop a mongodb deployment along with the server.

--- bin/start-server.js
+++ bin/start-server.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
+var runner = require('mongodb-runner');
 
-/*eslint no-console:0*/
-require('./serverctl').start(function(err) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-    return;
-  }
-  process.exit(0);
+runner({
+  action: 'start'
+}, function() {
+  /*eslint no-console:0*/
+  require('./serverctl').start(function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+      return;
+    }
+    process.exit(0);
+  });
 });
--- bin/stop-server.js
+++ bin/stop-server.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
+var runner = require('mongodb-runner');
 
-/*eslint no-console:0*/
-require('./serverctl').stop(function(err) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-    return;
-  }
-  process.exit(0);
+runner({
+  action: 'stop'
+}, function() {
+  /*eslint no-console:0*/
+  require('./serverctl').stop(function(err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+      return;
+    }
+    process.exit(0);
+  });
 });
